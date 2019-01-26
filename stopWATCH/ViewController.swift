da62c761f494e0725c9ed3dc5d48a76d9af9178e
Orlando <ortegado@mcmaster.ca>
2019-01-26 16:04:25 -0500

    DeltaHacks app

---
+++ stopWATCH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stopWATCH
+//
+//  Created by Orlando Ortega on 2019-01-26.
+//  Copyright © 2019 Orlando Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
