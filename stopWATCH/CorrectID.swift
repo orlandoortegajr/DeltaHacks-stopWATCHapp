d848a279119e2092a3606ba5c055eeaf5129ff53
Orlando <ortegado@mcmaster.ca>
2019-01-26 21:14:49 -0500

    send gps signals, send credentials to server, TODO: set up UI

--- stopWATCH/CorrectID.swift
+++ stopWATCH/CorrectID.swift
@@ -8,11 +8,70 @@
 
 import UIKit
 import Alamofire
+import CoreLocation
 
-class CorrectIDViewController: UIViewController {
+class CorrectIDViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var driverID: String?
+    var locationManager: CLLocationManager?
+    var parameters: [String: Any]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        
+        locationManager?.requestAlwaysAuthorization()
+        
+        locationManager?.startUpdatingLocation()
+        locationManager?.distanceFilter = 4
+    }
+    
+    //confirms user accepted to be tracked
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways{
+            //acceptance received
+            print("User Accepted")
+        } else {
+            //acceptance not received
+            print("Location not accepted")
+        }
+    }
+    
+    //updates user location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for currentLocation in locations{
+            print("New location is \(locations)")
+            makeRequest(location: currentLocation)
+        }
+    }
+    
+    func makeRequest(location: CLLocation){
+        let urlString = "https://sigma-myth-229819.appspot.com/link"
+        if let driverID = driverID {
+            parameters = ["driverID": driverID, "lat": location.coordinate.latitude, "lon": location.coordinate.longitude]
+        } else {
+            print("Invalid driverID")
+        }
+        
+        
+        Alamofire.request(urlString, method: .post, parameters: parameters,encoding: JSONEncoding.default, headers: nil).responseJSON { response in
+            switch response.result {
+            case .success:
+                print(response)
+                
+                //TODO: Confirm response in order to properly authenticate user
+                
+                
+                break
+                
+            case .failure(let error):
+                print(error)
+                
+            }
+        }
     }
     
 }
--- stopWATCH/LinkDriverToTruck.swift
+++ stopWATCH/LinkDriverToTruck.swift
@@ -12,40 +12,57 @@ import Alamofire
 class LinkDriverToTruckViewController: UIViewController, UITextFieldDelegate {
     
     
+    @IBOutlet weak var driverIDTextField: UITextField!
     @IBOutlet weak var truckIDTextField: UITextField!
     
+    var driverID = ""
     var truckID = ""
+    var parameters: [String: Any]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         truckIDTextField.delegate = self
+        driverIDTextField.delegate = self
     }
     
-    func makeRequest(truckID: String){
-        let urlString = "https://stackoverflow.com/post"
+    func makeRequest(driverID: String, truckID: String){
+        let urlString = "https://sigma-myth-229819.appspot.com/link"
+        parameters = ["driverID": driverID, "truckID": truckID]
         
-        Alamofire.request(urlString, method: .post, parameters: ["truckID": truckID],encoding: JSONEncoding.default, headers: nil).responseJSON { response in
+        
+        Alamofire.request(urlString, method: .post, parameters: parameters,encoding: JSONEncoding.default, headers: nil).responseJSON { response in
             switch response.result {
             case .success:
                 print(response)
                 
-                //TODO: Confirm response in order to properly authenticate user
-                self.performSegue(withIdentifier: "CorrectID", sender: self)
+                if let error = response.error{
+                    print(error)
+                    break
+                } else {
+                    //TODO: Confirm response in order to properly authenticate user
+                    let vc = CorrectIDViewController()
+                    vc.driverID = driverID
+                    self.performSegue(withIdentifier: "CorrectID", sender: self)
+                }
                 
                 break
                 
             case .failure(let error):
                 print(error)
                 
-                self.performSegue(withIdentifier: "IncorrectID", sender: self)
+                //TODO: Display error alert to user
             }
         }
     }
     
-    func authenticateTruck(){
+    func authenticateDriverAndTruck(){
         if let truckID = truckIDTextField.text {
-            makeRequest(truckID: truckID)
+            if let driverID = driverIDTextField.text{
+                makeRequest(driverID: driverID, truckID: truckID)
+            } else {
+                print("Driver text field is empty")
+            }
         } else {
             print("Truck text field is empty")
         }
@@ -54,7 +71,7 @@ class LinkDriverToTruckViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBAction func linkToTruckPressed(_ sender: Any) {
-        authenticateTruck()
+        authenticateDriverAndTruck()
     }
     
     //keyboard closes when return is pressed
